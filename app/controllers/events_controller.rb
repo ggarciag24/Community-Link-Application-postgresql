8e801ef86cd2164dc37d932846bc0be5cc85f345
German Garcia Gonzalez <ggarciag.business@outlook.com>
2019-11-03 15:02:37 -0500

    posting event works

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,22 @@
 class EventsController < ApplicationController
 
-def index
-  events = Event.all
+  def index
+    events = Event.all
 
-  render json:events
-end
+    render json: events
+  end
+
+  def create
+    name = params['name']
+    focus = params['focus']
+    location = params['location']
+    start_time = params['start_time']
+    end_time = params['end_time']
+    description = params['description']
+    host_id = params['host_id']
+
+    newEvent = Event.create({name: name, focus: focus, location: location, start_time: start_time, end_time: end_time, description: description, host_id: host_id})
+    render json: newEvent
+  end
 
 end
